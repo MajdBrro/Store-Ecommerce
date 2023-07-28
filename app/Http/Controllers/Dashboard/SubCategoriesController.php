c9be98b9a1784a3cc7905317502d701a9c69def0
Majd Brro <majd.brro2@gmail.com>
2023-07-28 11:14:27 +0300

    =end Sub-Categories

--- app/Http/Controllers/Dashboard/SubCategoriesController.php
+++ app/Http/Controllers/Dashboard/SubCategoriesController.php
@@ -8,52 +8,56 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Symfony\Component\Console\Input\Input;
 
-class MainCategoriesController extends Controller
+class SubCategoriesController extends Controller
 {
     public function index(){
-        $categories = Category::parent()-> orderBy('id','DESC')-> whereNull('parent_id') ->paginate(PAGINATION_COUNT);
-        return view('dashboard.categories.index',compact('categories'));
+        $categories = Category::child()-> orderBy('id','DESC')-> whereNotNull('parent_id') ->paginate(PAGINATION_COUNT);
+        return view('dashboard.subcategories.index',compact('categories'));
     }
 ###################################################################################################
 public function edit($id){
-    $category=Category::findorfail($id);
-    return view('dashboard.categories.edit',compact('category'));
+    $subcategory=Category::findorfail($id);
+    $maincategories= Category::parent()-> orderBy('id','DESC')-> whereNull('parent_id') ->paginate(PAGINATION_COUNT);
+    return view('dashboard.subcategories.edit',compact('subcategory','maincategories'));
 }
 ###################################################################################################
 public function update(Request $request, $id){
     #validation
     $category=Category::findorfail($id);
     if(!$category)
-    return redirect()->route('admin.maincategories') -> with(['error' => 'it is not found']);
+    return redirect()->route('admin.subcategories') -> with(['error' => 'it is not found']);
     $category->update([
         // 'slug' => $request->slug,
         'is_active' => $request -> is_active == 1 ? "1" : "0",
+        'parent_id' => $request -> input('parent_id'),
     ]);
     $category -> name = $request -> name;// يونيك name هنا يوجد ملاحظة انو الباكج نفسها تعتبر ال
     // $category -> slug  = str_replace(' ', '-', $request -> name); 
     $category -> save();
-    return redirect()->route('admin.maincategories') -> with(['success' => 'it was done successful']);
+    return redirect()->route('admin.subcategories') -> with(['success' => 'it was done successful']);
     
 }
 ###################################################################################################
 public function create(){
-    return view('dashboard.categories.create');
+    $categories = Category::parent()-> orderBy('id','DESC')-> whereNull('parent_id') ->paginate(PAGINATION_COUNT);
+    return view('dashboard.subcategories.create',compact('categories'));
 }
 ###################################################################################################
-public function store(MainCategoryRequest $request){
+public function store(Request $request){
     // return $request;
     Category::create([
         'name' => $request -> input('name'),
         'slug' => $request -> input('slug'),
+        'parent_id' => $request -> input('parent_id') ,
         'is_active' => $request -> is_active == 1 ? "1" : "0",
     ]);
-    return redirect()->route('admin.maincategories') -> with(['success' => 'it was added successful']);
+    return redirect()->route('admin.subcategories') -> with(['success' => 'it was added successful']);
 }
 ###################################################################################################
 public function delete($id){
     $delcat=Category::findorfail($id);
     $delcat -> delete();
-    return redirect()->route('admin.maincategories') -> with(['success' => __('admin.it_was_deleted_successfully')]);
+    return redirect()->route('admin.subcategories') -> with(['success' => __('admin.it_was_deleted_successfully')]);
 }
 ###################################################################################################
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,6 +17,19 @@ class Category extends Model
     protected $casts = [
         'is_active' => 'boolean',
     ];
+
+    public function scopeParent($query){
+        return $query -> whereNull('parent_id');
+    }
+    public function scopeChild($query){
+        return $query -> whereNotNull('parent_id');
+    }
+
+   
+
+
+
+
     public function getActive(){
         return $this -> is_active == 0 ? __('admin.un_available') : __('admin.available');
         // return  $this -> is_active  == 0 ?  ' مفعل'   : 'غير مفعل' ;
